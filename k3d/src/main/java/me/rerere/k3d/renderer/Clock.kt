c6da73090418f0813277862cac0d373c292d875e
re-ovo <qq1609403959@outlook.com>
2023-10-06 14:35:55 +0800

    perf: avoid Clock memory allocating

--- k3d/src/main/java/me/rerere/k3d/renderer/Clock.kt
+++ k3d/src/main/java/me/rerere/k3d/renderer/Clock.kt
@@ -1,15 +1,12 @@
 package me.rerere.k3d.renderer
 
-import kotlin.time.TimeSource
-
 class Clock {
-    private val timeSource = TimeSource.Monotonic
-    private var lastTime = timeSource.markNow()
+    private var lastTime = System.nanoTime()
     private var deltaTime = 0.0f
 
     fun tick() {
-        val now = timeSource.markNow()
-        deltaTime = (now - lastTime).inWholeMilliseconds / 1000.0f
+        val now = System.nanoTime()
+        deltaTime = (now - lastTime) / 1000000000.0f
         lastTime = now
     }
 
